99533c447641285b73a0f14ed5c57af1ec874436
jr <jrigler@student.tugraz.at>
2025-06-03 21:01:48 +0200

    change loss_calc functions

--- loss_calculation_header.py
+++ loss_calculation_header.py
@@ -1,26 +1,23 @@
-def compute_losses(x_b, x, absolute = True, relative = False):
-    if relative == True:
-        absolute = False
-
-    x_i = x['combined_shocks']
-                        
-    if absolute:
-        loss = (x_b['base'] - x_i).fillna(0)     # Absolute loss calculation
+def compute_losses(x_b, x, relative = False):
+    l   = x.copy() # Dataframe to store losses
+    x_i = x['combined_shocks'] # Extract values to transform
 
     if relative:
-        loss = (1 - x_i / x_b['base']).fillna(0) # Realative loss calculation
-        loss = loss.clip(lower = -1)             # Data manipulation
-
-    l = x.copy()
-    l['combined_shocks'] = loss
+        l      = l.rename(columns = {'combined_shocks': 'relative_losses'}) # Rename column
+        losses = (1 - x_i / x_b['base']).fillna(0).clip(lower = -1)  # Realative loss calculation and Manipulation
+        l['relative_losses'] = losses
+        return l
+              
+    l = l.rename(columns = {'combined_shocks': 'absolute_losses'}) # Rename column
+    losses = (x_b['base'] - x_i).fillna(0) # Absolute loss calculation
+    l['absolute_losses'] = losses
     return l
 
-def save_calculation(folder, calculation, absolute = True, relative = False):
-    if relative == True:
-        absolute = False
+def save_calculation(folder, calculation, relative = False):
 
-    if absolute:
-        calculation.to_csv(folder + 'AL-combined_shocks_comp.csv')
     if relative:
         calculation.to_csv(folder + 'RL-combined_shocks_comp.csv')
+        return
+    
+    calculation.to_csv(folder + 'AL-combined_shocks_comp.csv')
     return
