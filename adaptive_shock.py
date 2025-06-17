81f38df3ac20a9aaddeef42d813f2262c3877f77
silvestl <lorenzo.silvestri@uni-graz.at>
2025-06-17 11:01:25 +0200

    adaptive_shock.py
    Updated with production cap

--- adaptive_shock.py
+++ adaptive_shock.py
@@ -4,126 +4,66 @@
 #  ADAPTED BY CSM_SEXY_GRP_ - 2025, ORIGIN: SOPHIA BAUM - 2024
 
 
-### IMPORTS ###
+### IMPORTS ### PAK RUS HOA URU ALL
 
-import warnings
-warnings.filterwarnings("ignore")
-
-import pandas as pd
-from pandas import IndexSlice as idx
-
-import scipy.io as io
-import scipy.sparse as sprs
-import numpy as np
+from input.shock_input_data import *
 
 
 ### PARAMETERS ###
 
+scenario = 'PAK'                    # specify scenario
+production_cap = True               # turn on / off global production cap
+compensation = True                 # turn adaptation on
+tau = 10                            # number of iterations
+
 input_folder  = './input/'          # folder with parameters and input data
 output_folder = './results/'        # folder to write results to
-
-tau = 10                            # number of iterations
-compensation = True                 # turn adaptation on
 
 limit_abs_sim = 1000                # Event limits
 limit_rel_sim = 0.26
 limit_dev_sim = 0.32
 
 
-
-mu = -0.5                                                # Assumption
-
-def phi(phi_0, mu, t):                                      
-    return np.round(phi_0 * np.exp(mu * t), 2)           # Assumed exponential decay of shock intensity over time
-
-# Shock scenarios
-shock_sectors_pakistan = [('Pakistan','Rice and products'),
-                            ('Pakistan','Cottonseed'),
-                            ('Pakistan','Rape and Mustardseed'),
-                            ('Pakistan', 'Peas'),
-                            ('Pakistan', 'Dates')]
-
-phi_0_pakistan = [0.2146, 0.4124, 0.27, 0.29, 0.7274]
-
-shock_sectors_russia   = [('Russian Federation', 'Wheat and products'),
-                            ('Russian Federation', 'Barley and products'),
-                            ('Russian Federation', 'Cereals, Other'),
-                            ('Russian Federation', 'Maize and products'),
-                            ('Russian Federation', 'Oats'),
-                            ('Russian Federation', 'Peas'),
-                            ('Russian Federation', 'Potatoes and products'),
-                            ('Russian Federation', 'Rye and products')]
-
-phi_0_russia = [0.3276, 0.5330, 0.3985, 0.2216, 0.4039, 0.2344, 0.3208, 0.6225]
-
-shock_sectors_HOA = [('Kenya', 'Sugar cane'),
-                     ('Ethiopia', 'Sorghum and products'),
-                     ('Kenya', 'Tea (including mate)'),
-                     ('Kenya', 'Tomatoes and products'),
-                     ('Ethiopia', 'Milk - Excluding Butter'),
-                     ('Ethiopia', 'Sugar cane'),
-                     ('Ethiopia', 'Sweet potatoes'),
-                     ('Kenya', 'Pineapples and products'),
-                     ('Ethiopia', 'Millet and products'),
-                     ('Kenya', 'Sorghum and products'),
-                     ('Kenya', 'Eggs'),
-                     ('Kenya', 'Wheat and products'),
-                     ('Ethiopia', 'Groundnuts'),
-                     ('Kenya', 'Millet and products'),
-                     ('Ethiopia', 'Rice and products'),
-                     ('Ethiopia', 'Sesame seed'),
-                     ('Ethiopia', 'Honey'),
-                     ('Ethiopia', 'Onions'),
-                     ('Kenya', 'Lemons, Limes and products'),
-                     ('Kenya', 'Onions'),
-                     ('Kenya', 'Coconuts - Incl Copra'),
-                     ('Djibouti', 'Beans'),
-                     ('Somalia', 'Bovine Meat')]
-
-phi_0_HOA = [0.1842, 0.1123, 0.1548, 0.4432, 0.0972, 0.1883, 0.19, 0.7051, 0.0973, 0.3685, 0.0883,
-             0.2353, 0.4392, 0.4329, 0.2469, 0.2315, 0.3458, 0.1257, 0.4019,
-             0.1719, 0.2577, 0.2516, 0.0385] #HOA values averages per year 2021-2023 - need to recheck these values
-
-shock_sectors_URU = [('Uruguay', 'Soyabeans'),
-                     ('Uruguay', 'Maize and products'),
-                     ('Uruguay', 'Milk - Excluding Butter'),
-                     ('Uruguay', 'Sorghum and products'),
-                     ('Uruguay', 'Lemons, Limes and products'),
-                     ('Uruguay', 'Rice and products'),
-                     ('Uruguay', 'Oranges, Mandarines')]
-
-phi_0_uruguay = [0.3795, 0.3455, 0.9296, 0.2178, 0.3683, 0.9790, 0.1889] #Uruguay values averages per year 2021-2023
-
-# Construct shock scaling
-#scenario = 'Pakistan_floods'
-#shock_sectors = shock_sectors_pakistan
-#shock_scaling = np.zeros(( len(shock_sectors_pakistan), tau )) #[1 - phi(t) for t in range(tau)] # Create values to scale production-output
-#for row_index, row in enumerate(shock_scaling):
-#    shock_scaling[row_index, : ] = [1 - phi(phi_0_pakistan[row_index], mu, t) for t in range(tau)]
-
-#combined shock scenario
-#scenario = "Combined_PAKRUS"  # Combined scenario for Pakistan and Russia
-#combined_shock_scenario = "PAKRUS"
-#shock_sectors = shock_sectors_pakistan + shock_sectors_russia
-#phi_0_combined = phi_0_pakistan + phi_0_russia
-#shock_scaling = np.zeros(( len(shock_sectors), tau )) 
-
-scenario = "ALL"  # Combined scenario for all regions
-combined_shock_scenario = "ALL"
-shock_sectors = shock_sectors_pakistan + shock_sectors_russia + shock_sectors_HOA + shock_sectors_URU
-phi_0_combined = phi_0_pakistan + phi_0_russia + phi_0_HOA + phi_0_uruguay
-shock_scaling = np.zeros(( len(shock_sectors), tau )) 
-
-
-
-# Iterate through the combined phi_0 values to populate the shock_scaling matrix
-for row_index, phi_val in enumerate(phi_0_combined):
-    shock_scaling[row_index, :] = [1 - phi(phi_val, mu, t) for t in range(tau)]
-
-
 ### LOADING DATA ###
 
-                        # Load information
+                        # Select matching shock scenario
+match scenario:
+    case "PAK":
+        shock_sectors = shock_sectors_PAK
+        phi_0 = phi_0_PAK 
+
+    case "RUS": 
+        shock_sectors = shock_sectors_RUS
+        phi_0 = phi_0_RUS
+
+    case "HOA":
+        shock_sectors = shock_sectors_HOA
+        phi_0 = phi_0_HOA
+        
+    case "URU":
+        shock_sectors = shock_sectors_URU
+        phi_0 = phi_0_URU
+
+    case _:
+        shock_sectors = shock_sectors_PAK + shock_sectors_RUS + shock_sectors_HOA + shock_sectors_URU 
+        phi_0 = phi_0_PAK + phi_0_RUS + phi_0_HOA + phi_0_URU
+
+                        # Construct shock scaling
+shock_scaling = np.zeros((len(shock_sectors), tau )) #[1 - phi(t) for t in range(tau)] # Create values to scale production-output
+
+for row_index, row in enumerate(shock_scaling):
+    shock_scaling[row_index, : ] = [1 - phi(phi_0[row_index], mu, t) for t in range(tau)]
+
+
+# global food production cap 16 billion t (rounded total t of 2023 FAO)
+productioncap = 16e9
+
+# Construct shock scaling
+shock_scaling = np.zeros(( len(shock_sectors), tau )) #[1 - phi(t) for t in range(tau)] # Create values to scale production-output
+for row_index, row in enumerate(shock_scaling):
+    shock_scaling[row_index, : ] = [1 - phi(phi_0[row_index], mu, t) for t in range(tau)]
+
+                        # Load further information
 io_codes = pd.read_csv(input_folder + 'io_codes_alph.csv').drop('Unnamed: 0', axis = 1)
 su_codes = pd.read_csv(input_folder + 'su_codes_alph.csv').drop('Unnamed: 0', axis = 1)
 
@@ -229,6 +169,7 @@ for t in range(tau):
     if t == 0:
         x = x+xstartstock
 
+                        # Update time series
     x_timetrace_base[:,t] = x.toarray()[:, 0]
 
                         # Store
@@ -281,15 +222,23 @@ for t in range(tau):
 
                         # Apply shocks
     for sector_id in sector_ids:
-        sector       = sector_ids.index(sector_id)
+        sector = sector_ids.index(sector_id)
         o[sector_id] = shock_scaling[sector, t] * o[sector_id]
-                
-                
+                        
                         # Trade
     h = T_shock @ (eta_exp_shock.multiply(xs))
                 
                         # Summation 
     xs = o + h
+
+# global production cap:
+    if production_cap:
+        total_prod = xs.sum()
+# increase production cap by 1.1% per time step
+        productioncap *= 1.011
+        if total_prod > productioncap:
+            scaling = productioncap / total_prod
+            xs = xs.multiply(scaling)
 
     xs_timetrace[:, t] = xs.toarray()[:, 0]
 
